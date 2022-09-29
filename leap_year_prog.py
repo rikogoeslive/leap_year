b1ecad50f82c102df600cc9b22acf1b5e0faf853
Riko <mntsklp@gmail.com>
2022-09-29 16:08:18 +0300

    added function for calculation in row 2

--- leap_year_prog.py
+++ leap_year_prog.py
@@ -16,6 +16,7 @@ submenu.add_cascade(label="Kaip tai apskaičiuoti ?!", command=howto)
 submenu.add_separator()
 submenu.add_cascade(label="Išeiti iš programos", command=quit)
 
+
 def calculation():
     years = int(e_year.get())
     if (years % 400 == 0) or (years %100 != 0 and years %4 ==0): 
@@ -23,6 +24,16 @@ def calculation():
     else:
         l_result["text"] = f"{years} metai nėra keliamieji!"
 
+def calculation_from_till():
+    year_from = int(e_year_from.get())
+    year_till = int(e_year_till.get())
+    if year_from <= year_till:
+        leap_years = [str(x) for x in range(year_from, year_till) if x % 4==0 and (x % 100 !=0 or x %400 ==0)]
+        leap_years[-1] += "."
+        l_result["text"] = (f"Štai sąrašas keliamųjų metų nuo {year_from} iki {year_till}:\n{(', '.join(leap_years))}")
+    else:
+        l_result["text"] = "Įvesti pirmieji metai negali būti didesni už antruosius <3!"
+       
 
 l_year = Label(mainwindow, text="Įveskite metus:")
 e_year = Entry(mainwindow)
@@ -31,8 +42,8 @@ l_year_from = Label(mainwindow, text="Įveskite metus nuo:")
 e_year_from = Entry(mainwindow)
 l_year_till = Label(mainwindow, text="iki:")
 e_year_till = Entry(mainwindow)
-button2 = Button(mainwindow, text="Patvirtinti")
-l_result = Label(mainwindow, text="", bd=5, relief=SUNKEN, anchor="w")
+button2 = Button(mainwindow, text="Patvirtinti", command=calculation_from_till)
+l_result = Label(mainwindow, text="", bd=5, relief=SUNKEN, anchor=W)
 
 
 l_year.grid(row=0, column=0)

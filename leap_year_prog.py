b9bc886ab3c5ced2fc81a8bbe6be7bc5303c5b6c
Riko <mntsklp@gmail.com>
2022-09-29 11:38:21 +0300

    added label, entry field and button

--- leap_year_prog.py
+++ leap_year_prog.py
@@ -2,6 +2,18 @@ from tkinter import *
 
 mainwindow = Tk()
 mainwindow.title("Keliamųjų metų programėlė")
-mainwindow.geometry('400x300')
+mainwindow.geometry("400x300")
+
+l_year = Label(mainwindow, text="Įveskite metus:")
+e_year = Entry(mainwindow)
+button = Button(mainwindow, text="Patvirtinti")
+
+l_year.grid(row=0, column=0)
+e_year.grid(row=0, column=1)
+button.grid(row=0, column=2)
+
+
+
+
 
 mainwindow.mainloop()

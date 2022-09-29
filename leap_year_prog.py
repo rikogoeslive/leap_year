f6ba97a475557e87b33dac20d8aa449aba7e8a63
Riko <mntsklp@gmail.com>
2022-09-29 12:24:13 +0300

    added funcion for calculation and result label.

--- leap_year_prog.py
+++ leap_year_prog.py
@@ -4,16 +4,29 @@ mainwindow = Tk()
 mainwindow.title("Keliamųjų metų programėlė")
 mainwindow.geometry("400x300")
 
+def calculation():
+    years = int(e_year.get())
+    if (years % 400 == 0) or (years %100 != 0 and years %4 ==0): 
+        l_result["text"] = f"{years} yra keliamieji metai!"
+    else:
+        l_result["text"] = f"{years} metai nėra keliamieji!"
+
+
 l_year = Label(mainwindow, text="Įveskite metus:")
 e_year = Entry(mainwindow)
-button = Button(mainwindow, text="Patvirtinti")
+button = Button(mainwindow, text="Patvirtinti", command=calculation)
+l_result = Label(mainwindow, text="", bd=5, relief=SUNKEN, anchor="w")
+
 
 l_year.grid(row=0, column=0)
 e_year.grid(row=0, column=1)
 button.grid(row=0, column=2)
-
-
-
+l_result.grid(row=1, columnspan=5, sticky=W+E)
 
 
 mainwindow.mainloop()
+
+
+
+
+
